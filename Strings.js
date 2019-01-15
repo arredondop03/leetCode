ff316970bc29925c301e471f7f261a0096b9c9d6
Paola Arredondo <arredondop03@gmail.com>
2019-01-15 13:16:57 -0500

    valid anagram

--- Strings.js
+++ Strings.js
@@ -2,36 +2,39 @@
 //Runtime 72 ms
 
 var reverseString = function(s) {
-  return s.split('').reverse().join('')
+  return s
+    .split("")
+    .reverse()
+    .join("");
 };
 
 //-----------------------------------Reverse Integer
 //Runtime 96 ms
 var reverse = function(x) {
-let num = x.toString().split('').reverse()
-console.log(num[0])
+  let num = x
+    .toString()
+    .split("")
+    .reverse();
+  console.log(num[0]);
 
-while (num[0] == 0) {
-  num.splice(0, 1)
-}
+  while (num[0] == 0) {
+    num.splice(0, 1);
+  }
 
-if (num.includes('-')) {
-  num.splice(num.indexOf('-'), 1)
-  num.unshift('-')
+  if (num.includes("-")) {
+    num.splice(num.indexOf("-"), 1);
+    num.unshift("-");
+  }
 
-}
-
-if (num.join('') > 2147483647 || num.join('') < -2147483647) {
-  return 0
-} else {
-  return num.join('')
-}
-}
-
+  if (num.join("") > 2147483647 || num.join("") < -2147483647) {
+    return 0;
+  } else {
+    return num.join("");
+  }
+};
 
 //better solution - found it in leetcode, here for reference :D
 //Runtime 72 ms
-
 
 // var reverse = function (num) {
 //   if (Number.MAX_SAFE_INTEGER < num || Number.MIN_SAFE_INTEGER > num) {
@@ -60,8 +63,6 @@ if (num.join('') > 2147483647 || num.join('') < -2147483647) {
 //       number = Math.floor(number / 10);
 //     }
 
-
-
 //   if (!isPositiveNumber) {
 //     result = result - (result * 2);
 //   }
@@ -73,29 +74,52 @@ if (num.join('') > 2147483647 || num.join('') < -2147483647) {
 //   return result;
 // };
 
-
 //-----------------------------------First Unique Character
 //Runtime 1308 ms
 
-let s = 'aaaddddas'
+// let s = 'aaaddddas'
 
-let firstUniqueCharacter = (s) =>{
-s = s.split('')
-for(let i = 0; i <s.length; i++){
-  let theChar = s[i]
-  s.splice(i,1)
-  if(s.indexOf(theChar) === -1){
-    return i
-  } else {
-    s.splice(i,0,theChar)
-  }
-}
-return -1
-}
+// let firstUniqueCharacter = (s) =>{
+// s = s.split('')
+// for(let i = 0; i <s.length; i++){
+//   let theChar = s[i]
+//   s.splice(i,1)
+//   if(s.indexOf(theChar) === -1){
+//     return i
+//   } else {
+//     s.splice(i,0,theChar)
+//   }
+// }
+// return -1
+// }
 
+// firstUniqueCharacter(s)
 
-firstUniqueCharacter(s)
+//-----------------------------------Valid Anagram
+//Runtime 76 ms
 
+// let isAnagram = function(s, t) {
+//   const obj = {};
+//   let word1 = s;
+//   let word2 = t;
 
-
-
+//   if (word1.length !== word2.length) {
+//     return false;
+//   } else {
+//     for (let i = 97; i < 123; i++) {
+//       obj[i] = 0;
+//     }
+//     for (let i = 0; i < word1.length; i++) {
+//       obj[word1.charCodeAt(i)] = obj[word1.charCodeAt(i)] + 1;
+//     }
+//     for (let i = 0; i < word2.length; i++) {
+//       obj[word2.charCodeAt(i)] = obj[word2.charCodeAt(i)] - 1;
+//     }
+//     for (let i = 97; i < 123; i++) {
+//       if (obj[i] !== 0) {
+//         return false;
+//       }
+//     }
+//   }
+//   return true;
+// };
